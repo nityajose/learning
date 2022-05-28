9271ae79b4e889c58b96779b1a9b2c87618c10ae
nityajose <nithya.jose.m@gmail.com>
2022-05-28 11:48:09 +0530

    small

---
+++ large.js
@@ -0,0 +1,9 @@
+let x=69
+let y=666
+if(x>y)
+{
+    console.log(x)
+}
+else{
+    console.log(y)
+}
---
+++ small.js
@@ -0,0 +1,10 @@
+let a=12
+let b=22
+if(a<b)
+{
+    console.log(a)
+}
+else
+{
+    console.log(b)
+}
